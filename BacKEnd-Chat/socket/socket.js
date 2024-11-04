b5979d092cece03820523a7169536b11c63b0ca5
mehedithe <mehedihasan66291@gmail.com>
2024-11-04 13:01:02 +0600

    update

--- BacKEnd-Chat/socket/socket.js
+++ BacKEnd-Chat/socket/socket.js
@@ -1,10 +1,10 @@
 import {Server} from "socket.io";
-import http from "http";
+import https from "https";
 import express from "express";
 
 const app = express();
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = new Server(server, {
     cors:{
      origin:['https://chat-app-user.vercel.app'],
