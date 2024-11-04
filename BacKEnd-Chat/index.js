1257711d949717edfa51cea5e3902ace952974b4
mehedithe <mehedihasan66291@gmail.com>
2024-11-04 09:56:25 +0600

    update

--- BacKEnd-Chat/index.js
+++ BacKEnd-Chat/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://chat-app-user.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
